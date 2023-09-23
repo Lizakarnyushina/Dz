e1eec79331395e08b8d3c8ee316379fdb62c5e9f
Lizakarnyushina <143872574+Lizakarnyushina@users.noreply.github.com>
2023-09-23 13:21:11 +0300

    Update 5.py

--- Домашняя 1/5.py
+++ Домашняя 1/5.py
@@ -1,17 +1,17 @@
 print('введите x, y')
 x = float(input())
 y = float(input())
-if (x == 0) and (y != 0):
+if x == 0 and y != 0:
     print('Точка лежит на ординате')
-elif (x != 0) and (y == 0):
+elif x != 0 and y == 0:
     print('Точка лежит на абсциссе')
-elif (x > 0) and (y > 0):
+elif x > 0 and y > 0:
     print('Точка лежит в первойчервети')
-elif (x < 0) and (y > 0):
+elif x < 0 and y > 0:
     print('Точка лежит во второй четверти')
-elif (x < 0) and (y < 0):
+elif x < 0 and y < 0:
     print('Точка лежит в третьей четверти')
-elif (x > 0) and (y < 0):
+elif x > 0 and y < 0:
     print('Точка лежит в четвёртой чертвети')
 else:
     print('Точка лежит в начале координат')
