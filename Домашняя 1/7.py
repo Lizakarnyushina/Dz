a18bd0a4bf024a650610490522829bf3cdc8e908
Lizakarnyushina <143872574+Lizakarnyushina@users.noreply.github.com>
2023-09-23 13:32:30 +0300

    Update 7.py

--- Домашняя 1/7.py
+++ Домашняя 1/7.py
@@ -2,7 +2,7 @@ print('Введите x, y')
 x = int(input())
 y = int(input())
 a = 0
-for i in range(x, y + 1):
-    if i % 5 == 0:
+for i in range(x, y + 1) :
+    if i % 5 == 0 :
         a += i
 print(a)
