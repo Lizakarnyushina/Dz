f936c86102de49f7bac56818bb93775faf240f04
Lizakarnyushina <143872574+Lizakarnyushina@users.noreply.github.com>
2023-09-30 13:15:54 +0300

    Delete Домашняя 2 directory

--- Домашняя 2/2.py
+++
@@ -1,5 +0,0 @@
-numbers = input("Введите список чисел через запятую: ").split(",")
-numbers = [int(num) for num in numbers]
-numbers.sort(reverse=True, key=lambda x: str(x))
-result = "".join(str(num) for num in numbers)
-print("Максимально возможное число:", result)
