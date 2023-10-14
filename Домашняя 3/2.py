ad393ad31187b7e99b12c93938831c2ba27d9f3b
Lizakarnyushina <143872574+Lizakarnyushina@users.noreply.github.com>
2023-10-14 14:27:04 +0300

    Update 2.py

--- Домашняя 3/2.py
+++ Домашняя 3/2.py
@@ -1,13 +1,11 @@
-n = (int(input('Введите число от 1 до 12: ')))
-def F(n):
-    if n in (1, 2, 12):
-        print('Зима')
-    elif n in (3, 4, 5):
-        print('Весна')
-    elif n in (6, 7, 8):
-        print('Лето')
-    elif n in (9, 10, 11):
-        print('Осень')
-    else:
-        print('Ошибка')
-F(n)
+def f(n):
+    if n in [1, 2, 12]:
+        return "зима"
+    if n in [3, 4, 5]:
+        return "весна"
+    if n in [6, 7, 8]:
+        return "лето"
+    return "осень"
+
+
+print(f(int(input())))
