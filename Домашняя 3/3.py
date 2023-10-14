db146d3c9f7ae836164d25236925356078832f24
Lizakarnyushina <143872574+Lizakarnyushina@users.noreply.github.com>
2023-10-14 14:20:25 +0300

    .

--- Домашняя 3/3.py
+++
@@ -1,7 +0,0 @@
-def fib(n: int):
-    f = [0, 1, 1]
-    for _ in range(n - 2):
-        f.append(f[-2] + f[-1])
-    return f[:n]
-
-print(fib(int(input())), sep=', ')
--- Домашняя 3/5.py
+++
@@ -1,7 +0,0 @@
-n = [int(n) for n in input().split()]
-
-def f(n):
-
-    return sum(n)/len(n)
-
-print(f(n))
