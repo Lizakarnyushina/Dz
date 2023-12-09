9fc0f155e94cb25ac7f02a162b68d5eeee6d41e9
Lizakarnyushina <143872574+Lizakarnyushina@users.noreply.github.com>
2023-12-09 14:09:13 +0300

    Update test_dz_7.py

--- tests/test_dz_7.py
+++ tests/test_dz_7.py
@@ -1,11 +1,17 @@
-import pytest
-from dz_7.one import bfs
-
-
-@pytest.mark.parametrize("graph, start, end, expected", [
-    ({1: [2, 3], 2: [4], 3: [5], 4: [], 5: []}, 1, 5, 2),
-    ({1: [2, 3], 2: [4], 3: [5], 4: [], 5: []}, 1, 4, 1),
-    ({1: [2, 3], 2: [4], 3: [5], 4: [], 5: []}, 1, 6, None)
-])
-def test_bfs(graph, start, end, expected):
-    assert bfs(graph, start, end) == expected
+import pytest  
+from dz_7.one import bfs  
+  
+def check(node, t): 
+    return node == t 
+ 
+ 
+@pytest.mark.parametrize("g, s, t, check, expected", [  
+    ({}, 1, 5, check, None), 
+    ({1: [], 2: [], 3: [],  4: [],  5: []}, 1, 5, check, None),  
+    ({1: [2, 3], 2: [4], 3: [5], 4: [], 5: []}, 1, 5, check, 2),  
+    ({1: [2, 3], 2: [4], 3: [5], 4: [], 5: []}, 1, 4, check, 2),  
+    ({1: [2, 3], 2: [4], 3: [5], 4: [], 5: []}, 1, 6, check, None),  
+    ({1: [2, 3], 2: [4], 3: [5], 4: [], 5: []}, 1, 1, check, 0),  
+])  
+def test_bfs(g, s, t, check, expected):  
+    assert bfs(g, s, t, check) == expected
