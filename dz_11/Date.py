27410bc23431161ee07b6a59667bb8b1d74a9c54
Lizakarnyushina <143872574+Lizakarnyushina@users.noreply.github.com>
2024-02-24 14:35:47 +0300

    Update Date.py

--- dz_11/Date.py
+++ dz_11/Date.py
@@ -4,21 +4,24 @@ class Date:
         self.month = month
         self.day = day
         self.output = None
-
+        if not self.validate():
+            raise ValueError("Некорректная дата при создании объекта")
+ 
     def input_date(self):
         year, month, day = map(int, input("Введите дату ГГГГ.ММ.ДД: ").split('.'))
         self.year = year
         self.month = month
         self.day = day
-        
-
+        if not self.validate():
+            raise ValueError ("Некорректная дата при создании объекта")
+ 
     def __str__(self):
         return f"{self.year}.{self.month}.{self.day}"
-    
+ 
     def validate(self):
         if self.year < 1 or self.month < 1 or self.month > 12 or self.day < 1:
             return False
-        
+ 
         if self.month in [1, 3, 5, 7, 8, 10, 12]:
             max_day = 31
         elif self.month in [4, 6, 9, 11]:
@@ -28,8 +31,8 @@ class Date:
                 max_day = 29
             else:
                 max_day = 28
-
+ 
         if self.day > max_day:
             return False
-        
+ 
         return True
