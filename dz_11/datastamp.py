144a4e6b0129cc6f8c5dbecc6720df9934a54cae
Lizakarnyushina <143872574+Lizakarnyushina@users.noreply.github.com>
2024-03-02 13:59:27 +0300

    Update datastamp.py

--- dz_11/datastamp.py
+++ dz_11/datastamp.py
@@ -1,7 +1,7 @@
 from dz_11.date import Date
 
 class DateStamp(Date):
-    def __init__(self, day=None, month=None, year=None):
+    def __init__(self, day, month, year):
         self.day = day
         self.month = month
         self.year = year
