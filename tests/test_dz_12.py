c1c6d888f15dbaf0a8c090d15b4580f980c9cda6
Lizakarnyushina <143872574+Lizakarnyushina@users.noreply.github.com>
2024-03-02 13:50:28 +0300

    Update test_dz_12.py

--- tests/test_dz_12.py
+++ tests/test_dz_12.py
@@ -1,14 +1,18 @@
 import pytest
 from datetime import datetime
+from freezegun import freeze_time
 from dz_11.datastamp import DateStamp
 
-
 @pytest.mark.parametrize("day, month, year", [
-    (datetime.now().day, datetime.now().month, datetime.now().year),
-    (1, 1, 2000),
+    (1, 1, 2022),
+    (None, 1, 2022),
+    (1, None, 2022),
+    (None, None, 2022),
+    (None, None, None)
 ])
+@freeze_time("2022-01-01")
 def test_datestamp_creation(day, month, year):
-    datestamp = DateStamp(day=day, month=month, year=year)
-    assert datestamp.day == day
-    assert datestamp.month == month
-    assert datestamp.year == year
+    ds = DateStamp(day=day, month=month, year=year)
+    assert ds.day == day if day is not None else 1
+    assert ds.month == month if month is not None else 1
+    assert ds.year == year if year is not None else 2022
