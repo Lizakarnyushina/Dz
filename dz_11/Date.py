43d3fda0f7499aac2412bf85bbabb1501aec90f3
Lizakarnyushina <143872574+Lizakarnyushina@users.noreply.github.com>
2024-03-16 17:57:14 +0300

    Update Date.py

--- dz_11/Date.py
+++ dz_11/Date.py
@@ -6,6 +6,7 @@ class Date:
         if not self.validate():
             raise ValueError("Некорректная дата при создании объекта")
  
+
     def input_date(self):
         year, month, day = map(int, input("Введите дату ГГГГ.ММ.ДД: ").split('.'))
         self.year = year
@@ -14,9 +15,14 @@ class Date:
         if not self.validate():
             raise ValueError ("Некорректная дата при создании объекта")
  
+
     def __str__(self):
         return f"{self.year}.{self.month}.{self.day}"
+    
+    def __repr__(self):
+        return f"{self.year}.{self.month}.{self.day}"
  
+
     def validate(self):
         if self.year < 1 or self.month < 1 or self.month > 12 or self.day < 1:
             return False
@@ -35,3 +41,58 @@ class Date:
             return False
  
         return True
+    
+
+    def __eq__(self, other):
+        return self.year == other.year and self.month == other.month and self.day == other.day
+
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+
+    def __add__(self, other):
+        new_year = self.year + other.year
+        new_month = self.month + other.month
+        new_day = self.day + other.day
+
+        while new_month > 12:
+            new_year += 1
+            new_month -= 12
+
+        while new_day > 28:  
+            max_day = 31 if new_month in [1, 3, 5, 7, 8, 10, 12] else 30
+            if new_month == 2:
+                max_day = 29 if (new_year % 4 == 0 and new_year % 100 != 0) or new_year % 400 == 0 else 28
+
+            if new_day > max_day:
+                new_day -= max_day
+                new_month += 1
+                if new_month > 12:
+                    new_year += 1
+                    new_month = 1
+
+        return Date(new_year, new_month, new_day)
+
+    def __sub__(self, other):
+        new_year = self.year - other.year
+        new_month = self.month - other.month
+        new_day = self.day - other.day
+
+        while new_month < 1:
+            new_year -= 1
+            new_month += 12
+
+        while new_day < 1:
+            new_month -= 1
+            if new_month < 1:
+                new_year -= 1
+                new_month = 12
+
+            max_day = 31 if new_month in [1, 3, 5, 7, 8, 10, 12] else 30
+            if new_month == 2:
+                max_day = 29 if (new_year % 4 == 0 and new_year % 100 != 0) or new_year % 400 == 0 else 28
+
+            new_day += max_day
+
+        return Date(new_year, new_month, new_day)
